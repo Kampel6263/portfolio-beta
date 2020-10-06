aa0623ff8ee4c39f8deb26479141d78b128a7528
Kampel6263 <andrian6263@gmail.com>
2020-10-06 13:34:50 +0300

    Update loadMoreItems.js

--- js/loadMoreItems.js
+++ js/loadMoreItems.js
@@ -1,31 +1,47 @@
 let preloader = document.getElementById('preloader');
 
-console.log(window.innerHeight)
+if(innerWidth <= 700){
+    document.getElementById('load__for__mobile').style.display = 'none';
+    document.getElementById('load__more').style.display = 'block';
+
+}
 
 const loadMore = () => {
 
-    preloader.style.display = 'block';
-    document.getElementById('portfolio').style.height = '119vh';
-    document.getElementById('load').style.display = 'none';
+    if(innerWidth <= 700){
+        document.getElementById('preloader__for__mobile').style.display = 'block';
+        document.getElementById('load').style.display = 'none';
 
-    document.getElementById('portfolio').style.transition = '.03s linear'
+        setTimeout(() => {
+            document.getElementById('preloader__for__mobile').style.display = 'none';
+            document.getElementById('load__for__mobile').style.display = 'block';
+            document.getElementById('hide').style.display = 'inline-block';
+        }, 800)
+    } else {
+        preloader.style.display = 'block';
+        document.getElementById('load').style.display = 'none';
 
-    setTimeout(() => {
-        preloader.style.display = 'none';
-        document.getElementById('load__more').style.display = 'flex';
-        document.getElementById('hide').style.display = 'inline-block';
-        document.getElementById('portfolio').style.transition = '.3s linear'
-
-
-    }, 800)
-
+        setTimeout(() => {
+            preloader.style.display = 'none';
+            document.getElementById('load__more').style.display = 'flex';
+            document.getElementById('hide').style.display = 'inline-block';
+        }, 800)
+    }
 
 }
 
 const hide = () => {
-    document.getElementById('load__more').style.display = 'none';
-    document.getElementById('hide').style.display = 'none';
-    document.getElementById('load').style.display = 'inline-block';
-    document.getElementById('portfolio').style.height = '80vh';
-    // preloader.style.display = ''
+
+    if(innerWidth<=700){
+        document.getElementById('load__for__mobile').style.display = 'none';
+        document.getElementById('hide').style.display = 'none';
+        document.getElementById('load').style.display = 'inline-block';
+    } else {
+        document.getElementById('load__more').style.display = 'none';
+        document.getElementById('hide').style.display = 'none';
+        document.getElementById('load').style.display = 'inline-block';
+    }
+
+
 }
+

a67c548947ef5d8de0718a77792748fffad85778
Kampel6263 <andrian6263@gmail.com>
2020-10-06 13:34:47 +0300

    Update header.js

--- js/header.js
+++ js/header.js
@@ -12,7 +12,12 @@ const fun1 = () => {
 const fun2 = () => {
     header.style.background = "none";
     header.style.position = 'absolute';
-    header.style.padding = '25px 0';
+    if(innerWidth <= 500){
+        header.style.padding = '7px 0';
+    } else {
+        header.style.padding = '25px 0';
+    }
+
     header.style.boxShadow = '0px 1px 10px 0px rgba(0,0,0,0)'
 }
 
